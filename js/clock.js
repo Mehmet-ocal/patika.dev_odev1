36e6291ae5ff3dea974a4abd80f3fbb7008f405d
Mehmet Öcal <mehmettoocal@gmail.com>
2023-07-18 13:13:51 +0300

    Saat verisi güncellendi

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,14 @@ let name = prompt ("Adınızı Giriniz: ")
 document.getElementById("myName").innerHTML = name;
 
 function showTime() {
+  let clockElement = document.getElementById("myClock");
+
+  setInterval(() => {
     let currentDate = new Date();
     let currentTime = currentDate.toLocaleTimeString();
-    let currentDateTime = currentDate.toLocaleDateString() + " " + currentTime;
-    
-    document.getElementById("myClock").textContent = currentDateTime;
-  }
-  
-  showTime();
+    let currentDateFormatted = currentDate.toLocaleDateString();
+    clockElement.textContent = currentTime + " " + currentDateFormatted;
+  }, 1000);
+}
+
+showTime();
